eab521bf597f7e203f9d8306098a8c818d902b9b
IsabelSilva-98 <maria.isabel.silva.98@gmail.com>
2023-05-05 11:57:40 +0100

    já estou farta disto

--- Conta Bancaria - resolução/Conta Bancaria.cs
+++ Conta Bancaria - resolução/Conta Bancaria.cs
@@ -6,8 +6,29 @@ using System.Threading.Tasks;
 
 namespace Conta_Bancaria___resolução
 {
-    i
-        // TESTAR O PULL-------------------------------------------------------------------------------------
+    internal class Conta_Bancaria
+    {
+        // Definição de propriedades
+
+        public int NumeroConta { get; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; private set; }
+
+        // Construtores
+
+        public Conta_Bancaria(int numero, string titular) // Este construtor permite instanciar contas bancarias apenas com o numero de conta e o seu titular
+        {
+            NumeroConta = numero;
+            NomeTitular = titular;
+        }
+
+        public Conta_Bancaria(int numero, string titular, double saldo): this (numero, titular)  //Este construtor permite instanciar uma conta a partir do seu numero de conta, titular e também o seu saldo inicial
+        {
+            Saldo = saldo;
+        }
+
+        // Secção de Métodos
+
         public void Deposito (double quantia) // Este é o método que permite depositar dinheiro na conta
         {
             Saldo += quantia;
